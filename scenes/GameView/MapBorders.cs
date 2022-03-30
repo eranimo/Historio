d6104b4b343c846cfcbbb92e5da13fe7efce5fce
Kaelan Cooter <dcgamers@gmail.com>
2022-03-29 21:59:03 -0400

    MapBorders renders areas

--- scenes/GameView/MapBorders.cs
+++ scenes/GameView/MapBorders.cs
@@ -1,15 +1,30 @@
 using Godot;
 using System;
 
+/**
+Territories are groups of cells 
+Areas are subgroups of territories
+*/
 public class MapBorders : Polygon2D {
 	private GameMap gameMap;
-
-	public override void _Ready() {
-	}
-
 	private ShaderMaterial shader {
 		get { return (this.Material as ShaderMaterial); }
 	}
+	private int? _selectedTerritory = null;
+	private Image hexTerritoryColorImage;
+	private ImageTexture hexTerritoryColors;
+	private ImageTexture hexAreaColors;
+	private Image hexAreaColorImage;
+
+	public int? selectedTerritory {
+		get { return _selectedTerritory; }
+		set {
+			_selectedTerritory = value;
+			this.shader.SetShaderParam("selectedTerritory", value);
+		}
+	}
+
+	public override void _Ready() {}
 
 	public void RenderMap(GameMap gameMap) {
 		this.gameMap = gameMap;
@@ -25,36 +40,55 @@ public class MapBorders : Polygon2D {
 		this.shader.SetShaderParam("gridSize", worldSize.ToVector());
 		this.shader.SetShaderParam("containerSize", containerSize);
 
-		this.UpdateTerritoryMap();
+		this.setupMap();
+
+		this.updateTerritoryMap();
+		this.updateAreaMap();
 	}
 
-	private int? _selectedTerritory = null;
-	public int? selectedTerritory {
-		get { return _selectedTerritory; }
-		set {
-			_selectedTerritory = value;
-			this.shader.SetShaderParam("selectedTerritory", value);
-		}
-	}
-
-	private void UpdateTerritoryMap() {
+	public void setupMap() {
 		var worldSize = gameMap.game.manager.state.worldSize;
-		Image hexTerritoryColorImage = new Image();
+		hexTerritoryColorImage = new Image();
 		hexTerritoryColorImage.Create(worldSize.col, worldSize.row, false, Image.Format.Rgbaf);
+		hexAreaColorImage = new Image();
+		hexAreaColorImage.Create(worldSize.col, worldSize.row, false, Image.Format.Rgbaf);
 
+		hexTerritoryColors = new ImageTexture();
+		hexAreaColors = new ImageTexture();
+	}
+
+	private void updateTerritoryMap() {
 		hexTerritoryColorImage.Lock();
 
-		var c1 = Color.FromHsv(0.1f, 0.9f, 0.8f, 1f / 10_000);
-		var c2 = Color.FromHsv(0.2f, 0.9f, 0.8f, 2f / 10_000);
-		hexTerritoryColorImage.SetPixel(1, 1, c1);
-		hexTerritoryColorImage.SetPixel(1, 2, c1);
-		hexTerritoryColorImage.SetPixel(2, 3, c2);
-		hexTerritoryColorImage.SetPixel(3, 3, c2);
-		hexTerritoryColorImage.SetPixel(3, 4, c2);
+		var t1 = Color.FromHsv(0.1f, 0.9f, 0.8f, 1f / 10_000);
+		var t2 = Color.FromHsv(0.2f, 0.9f, 0.8f, 2f / 10_000);
+		hexTerritoryColorImage.SetPixel(1, 1, t1);
+		hexTerritoryColorImage.SetPixel(1, 2, t1);
+		hexTerritoryColorImage.SetPixel(2, 3, t2);
+		hexTerritoryColorImage.SetPixel(3, 3, t2);
+		hexTerritoryColorImage.SetPixel(3, 4, t2);
 
 		hexTerritoryColorImage.Unlock();
-		ImageTexture hexTerritoryColors = new ImageTexture();
+		
 		hexTerritoryColors.CreateFromImage(hexTerritoryColorImage);
 		this.shader.SetShaderParam("hexTerritoryColor", hexTerritoryColors);
+	}
+
+	private void updateAreaMap() {
+		hexAreaColorImage.Lock();
+
+		var a1 = Color.FromHsv(0f, 0f, 0f, 1f / 10_000);
+		var a2 = Color.FromHsv(0f, 0f, 0f, 2f / 10_000);
+		var a3 = Color.FromHsv(0f, 0f, 0f, 3f / 10_000);
+		hexAreaColorImage.SetPixel(1, 1, a1);
+		hexAreaColorImage.SetPixel(1, 2, a1);
+		hexAreaColorImage.SetPixel(2, 3, a2);
+		hexAreaColorImage.SetPixel(3, 3, a2);
+		hexAreaColorImage.SetPixel(3, 4, a3);
+
+		hexAreaColorImage.Unlock();
+		
+		hexAreaColors.CreateFromImage(hexAreaColorImage);
+		this.shader.SetShaderParam("hexAreaIDs", hexAreaColors);
 	}
 }
