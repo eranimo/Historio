95fe42482123900e229d2136e83df840f8961005
Kaelan Cooter <dcgamers@gmail.com>
2022-12-31 19:22:17 -0500

    3d spike: traditional non-globe terrain with chunks

---
+++ view/MapView/MapChunk.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+
+public partial class MapChunk : VisibleOnScreenNotifier3D {
+	[Export] public Vector2 WorldSize;
+	[Export] public Vector2 ChunkSize;
+	[Export] public Vector2 TerrainSize;
+	[Export] public Vector2 ChunkPosition;
+	[Export] public PlanetData PlanetData;
+	public ShaderMaterial TerrainMaterial;
+
+	public MeshInstance3D terrainChunk;
+	private MeshInstance3D waterChunk;
+	private bool hasRendered = false;
+
+	public override void _Ready() {
+		Connect("screen_entered", Callable.From(() => render()));
+		Connect("screen_exited", Callable.From(() => remove()));
+
+		terrainChunk = GetNode<MeshInstance3D>("%TerrainChunk");
+		waterChunk = GetNode<MeshInstance3D>("%WaterChunk");
+
+		setup();
+	}
+
+	private void render() {
+		terrainChunk.Show();
+		if (hasRendered) {
+			return;
+		}
+		hasRendered = true;
+		// GD.PrintS("Render chunk", ChunkPosition);
+
+		// setup();
+	}
+
+	private void remove() {
+		// GD.PrintS("Remove chunk", ChunkPosition);
+		terrainChunk.Show();
+	}
+
+	private void setup() {
+		(terrainChunk.Mesh as PlaneMesh).Size = ChunkSize;
+		(waterChunk.Mesh as PlaneMesh).Size = ChunkSize;
+		var material = ResourceLoader.Load<ShaderMaterial>("res://view/MapView/TerrainMaterial.tres").Duplicate() as ShaderMaterial;
+		material.SetShaderParameter("chunkPosition", ChunkPosition);
+		material.SetShaderParameter("worldSize", WorldSize);
+		material.SetShaderParameter("terrainSize", TerrainSize);
+		material.SetShaderParameter("chunkSize", ChunkSize);
+		material.SetShaderParameter("heightmap", PlanetData.Heightmap);
+		material.SetShaderParameter("normalmap", PlanetData.Normalmap);
+		material.SetShaderParameter("splatmap", PlanetData.Splatmap);
+		terrainChunk.MaterialOverride = material;
+	}
+}
--- view/MapView/Planet.cs
+++ view/MapView/Planet.cs
@@ -1,79 +1,116 @@
 using Godot;
 using System;
 
-public enum PlanetSize {
-	Small = 50,
-	Medium = 150,
-	Large = 250
+
+public partial class PlanetData : Resource {
+	[Export] public NoiseTexture2D Heightmap;
+	[Export] public NoiseTexture2D Normalmap;
+	[Export] public NoiseTexture2D Splatmap;
 }
 
-[Tool]
-public partial class Planet : MeshInstance3D {
-	private int seed = 123;
-	private PlanetSize planetSize = PlanetSize.Small;
-	private float radius = 1.0f;
-	private float waterHeight = 0.5f;
-	private MeshInstance3D water;
-	private PlanetShaderMaterial material;
-
-	[Export(PropertyHint.Enum)]
-	public PlanetSize PlanetSize {
-		get => planetSize;
-		set {
-			planetSize = value;
-			generateMesh();
-		}
-	}
+public partial class Planet : Node3D {
 
 	[Export]
-	public int Seed {
-		get => seed;
-		set {
-			seed = value;
-			generateMesh();
-		}
+	private int seed = 123;
+
+	[Export]
+	private int chunkWidth = 5;
+
+	[Export]
+	private int chunkHeight = 5;
+
+	[Export]
+	private int worldWidth = 500;
+
+	[Export]
+	private int worldHeight = 250;
+
+	[Export]
+	private PlanetData planetData;
+
+	public int ChunkWidth { get => chunkWidth; set { chunkWidth = value; } }
+	public int ChunkHeight { get => chunkHeight; set { chunkHeight = value; generate(); } }
+	public int Seed { get => seed; set { seed = value; generate(); } }
+	public int WorldWidth { get => worldWidth; set { worldWidth = value; generate(); } }
+	public int WorldHeight { get => worldHeight; set { worldHeight = value; generate(); } }
+
+	public Planet() {
+		// generate();
 	}
 
-	[Export(PropertyHint.Range, "1.0,10.0")]
-	public float Radius { get => radius; set { radius = value; generateMesh(); } }
-
-	[Export(PropertyHint.Range, "0.0,1.0")]
-	public float WaterHeight { get => waterHeight; set { waterHeight = value; generateWater(); } }
-
 	public override void _Ready() {
+		generate();
+	}
+
+	private void generate() {
 		// RenderingServer.SetDebugGenerateWireframes(true);
 		// GetViewport().DebugDraw = Viewport.DebugDrawEnum.Wireframe;
 
-		var watch = System.Diagnostics.Stopwatch.StartNew();
-		GD.PrintS($"(Planet) Generate: {watch.ElapsedMilliseconds}ms");
-		generateMesh();
-		generateWater();
-	}
+		GD.PrintS("Generating chunks", chunkWidth, chunkHeight);
+		foreach (var child in GetChildren()) {
+			RemoveChild(child);
+		}
 
-	private void generateMesh() {
-		var watch = System.Diagnostics.Stopwatch.StartNew();
-		Mesh = new CubeSphere((int) PlanetSize, radius);
-		material = ResourceLoader.Load<PlanetShaderMaterial>("res://view/MapView/PlanetShaderMaterial.tres");
-		var height = (int) PlanetSize;
-		var width = height * 2;
-		material.GenerateTerrain(width, height, seed, waterHeight);
-		material.GenerateTexture(width, height, seed, waterHeight);
-		MaterialOverride = material;
-		GD.PrintS($"\tBuilding Planet mesh: {watch.ElapsedMilliseconds}ms");
-	}
+		var worldSize = new Vector2(worldWidth, worldHeight);
+		var chunkGridSize = new Vector2(chunkWidth, chunkHeight);
+		var chunkSize = worldSize / chunkGridSize;
+		var terrainSize = chunkGridSize * worldSize;
+		GD.PrintS("\t worldSize", worldSize);
+		GD.PrintS("\t terrainSize", terrainSize);
+		GD.PrintS("\t chunkSize", chunkSize);
 
-	private void generateWater() {
-		var watch = System.Diagnostics.Stopwatch.StartNew();
-		water = GetNode<MeshInstance3D>("Water");
-		var height = (int) PlanetSize;
-		var width = height * 2;
-		material.GenerateTexture(width, height, seed, waterHeight);
-		var waterMesh = new SphereMesh();
-		var waterHeightFixed = radius + waterHeight;
-		waterMesh.Radius = waterHeightFixed;
-		waterMesh.Height = waterHeightFixed * 2;
-		water.Mesh = waterMesh;
+		var noise = new FastNoiseLite();
+		noise.Frequency = 0.002f;
+		noise.Seed = seed;
 
-		GD.PrintS($"\tBuilding Planet water: {watch.ElapsedMilliseconds}ms");
+		var heightmap = new NoiseTexture2D();
+		heightmap.In3dSpace = true;
+		heightmap.Width = (int) worldSize.x;
+		heightmap.Height = (int) worldSize.y;
+		heightmap.Noise = noise;
+
+		var normalmap = heightmap.Duplicate() as NoiseTexture2D;
+		normalmap.AsNormalMap = true;
+
+		var splatmap = heightmap.Duplicate() as NoiseTexture2D;
+		var gradient = new Gradient();
+		gradient.SetColor(0, new Color(1f, 0, 0));
+		gradient.SetColor(1, new Color(0, 1f, 0));
+		splatmap.ColorRamp = gradient;
+
+		planetData = new PlanetData {
+			Heightmap = heightmap,
+			Normalmap = normalmap,
+			Splatmap = splatmap,
+		};
+
+		var heightmapDebug = GetParent().GetNode<TextureRect>("UI/Heightmap");
+		heightmapDebug.Texture = heightmap;
+		heightmapDebug.Size = worldSize / 2f;
+
+		var normalmapDebug = GetParent().GetNode<TextureRect>("UI/Normalmap");
+		normalmapDebug.Texture = normalmap;
+		normalmapDebug.Size = worldSize / 2f;
+
+		var splatmapDebug = GetParent().GetNode<TextureRect>("UI/Splatmap");
+		splatmapDebug.Texture = splatmap;
+		splatmapDebug.Size = worldSize / 2f;
+
+		var terrainChunkScene = ResourceLoader.Load<PackedScene>("res://view/MapView/TerrainChunk.tscn");
+		for (int x = 0; x < ChunkWidth; x++) {
+			for (int y = 0; y < ChunkHeight; y++) {
+				var chunk = terrainChunkScene.Instantiate<MapChunk>();
+				var chunkPosition = new Vector2(x, y) * chunkSize;
+				chunk.Position = new Vector3(chunkPosition.x, 0, chunkPosition.y);
+				
+				GD.PrintS("Setup chunk", chunkPosition);
+				chunk.WorldSize = worldSize;
+				chunk.ChunkSize = chunkSize;
+				chunk.TerrainSize = terrainSize;
+				chunk.ChunkPosition = chunkPosition;
+				chunk.PlanetData = planetData;
+				AddChild(chunk);
+			}
+		}
 	}
 }
